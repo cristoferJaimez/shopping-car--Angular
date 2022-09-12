919d22745bec2070b551abecc75d3cae79572463
Cristofer Jaimez <cjaimez@CLOSEUPCOL>
2022-09-12 11:43:12 -0500

    invoice view , remove game, bugs

--- src/app/components/invoice-modal/invoice-modal.component.ts
+++ src/app/components/invoice-modal/invoice-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Games } from 'src/app/interface/games';
 
 @Component({
   selector: 'invoice-modal',
@@ -7,14 +8,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./invoice-modal.component.css']
 })
 export class InvoiceModalComponent implements OnInit {
+  buy_: Array<number> = [];
+  buy__: Array<Games> = [];
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    
+
+    this.invoice()
+    
   }
 
   navigation(){
     this.router.navigate(['/home'])
+  }
+
+  invoice(){
+    this.buy__ =  JSON.parse(localStorage.getItem('carrito')!)
+    this.buy_ =  JSON.parse(localStorage.getItem('buy')!)
+   
+    console.log(this.buy__);
+    
   }
 
 }
--- src/app/components/list-buy/list-buy.component.ts
+++ src/app/components/list-buy/list-buy.component.ts
@@ -2,18 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
   selector: 'list-buy',
-  templateUrl: './list-buy.component.html',
+  templateUrl: './list-buy.component.html', 
   styleUrls: ['./list-buy.component.css']
 })
 export class ListBuyComponent implements OnInit {
 
   constructor(private router: Router) { }
+  
+  buy_: Array<number> = [];
 
-  ngOnInit(): void {
+    ngOnInit(): void { 
+      this.buy_ =  JSON.parse(localStorage.getItem('buy')!)
   }
 
   navigation(){
-    this.router.navigate(['/invoicer'])
+     this.router.navigate(['/invoicer'])
   }
+
+
 
 }
